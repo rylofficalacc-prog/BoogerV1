0858c21ad60a0e9730e6f3a75f1b6664d584a35f
rylofficalacc-prog <rylofficalacc@gmail.com>
2026-04-14 20:38:52 -0400

    Update BoogerClient.java

--- src/main/java/com/boogerclient/BoogerClient.java
+++ src/main/java/com/boogerclient/BoogerClient.java
@@ -9,6 +9,7 @@ import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
 import net.fabricmc.fabric.api.client.rendering.v1.HudRenderCallback;
 import net.minecraft.client.option.KeyBinding;
+import net.minecraft.client.option.KeyBinding.Category;
 import net.minecraft.client.util.InputUtil;
 import org.lwjgl.glfw.GLFW;
 import org.slf4j.Logger;
@@ -27,6 +28,9 @@ public class BoogerClient implements ClientModInitializer {
     public static KeyBinding openMenuKey;
     public static KeyBinding toggleHudEditKey;
 
+    private static final Category BOOGERCLIENT_CATEGORY =
+        KeyBindingHelper.registerKeyCategory("key.categories.boogerclient");
+
     @Override
     public void onInitializeClient() {
         LOGGER.info("Booger Client initializing...");
@@ -40,14 +44,14 @@ public class BoogerClient implements ClientModInitializer {
             "key.boogerclient.menu",
             InputUtil.Type.KEYSYM,
             GLFW.GLFW_KEY_RIGHT_SHIFT,
-            "key.categories.misc"
+            BOOGERCLIENT_CATEGORY
         ));
 
         toggleHudEditKey = KeyBindingHelper.registerKeyBinding(new KeyBinding(
             "key.boogerclient.hud_edit",
             InputUtil.Type.KEYSYM,
             GLFW.GLFW_KEY_F6,
-            "key.categories.misc"
+            BOOGERCLIENT_CATEGORY
         ));
 
         HudRenderCallback.EVENT.register((drawContext, tickCounter) -> {
