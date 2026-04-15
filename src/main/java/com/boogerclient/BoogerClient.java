5c6440aefbe89c410e8e7100249d05f5332781fd
rylofficalacc-prog <rylofficalacc@gmail.com>
2026-04-14 20:51:56 -0400

    Update BoogerClient.java

--- src/main/java/com/boogerclient/BoogerClient.java
+++ src/main/java/com/boogerclient/BoogerClient.java
@@ -6,13 +6,7 @@ import com.boogerclient.pvp.PvpManager;
 import com.boogerclient.cosmetics.CosmeticsManager;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
-import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
 import net.fabricmc.fabric.api.client.rendering.v1.HudRenderCallback;
-import net.minecraft.client.option.KeyBinding;
-import net.minecraft.client.option.KeyBinding.Category;
-import net.minecraft.client.util.InputUtil;
-import net.minecraft.util.Identifier;
-import org.lwjgl.glfw.GLFW;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -26,12 +20,6 @@ public class BoogerClient implements ClientModInitializer {
     public static PvpManager pvpManager;
     public static CosmeticsManager cosmeticsManager;
 
-    public static KeyBinding openMenuKey;
-    public static KeyBinding toggleHudEditKey;
-
-    private static final Category CATEGORY =
-        Category.register(Identifier.of("boogerclient", "general"));
-
     @Override
     public void onInitializeClient() {
         LOGGER.info("Booger Client initializing...");
@@ -41,32 +29,12 @@ public class BoogerClient implements ClientModInitializer {
         pvpManager = new PvpManager();
         cosmeticsManager = new CosmeticsManager();
 
-        openMenuKey = KeyBindingHelper.registerKeyBinding(new KeyBinding(
-            "key.boogerclient.menu",
-            InputUtil.Type.KEYSYM,
-            GLFW.GLFW_KEY_RIGHT_SHIFT,
-            CATEGORY
-        ));
-
-        toggleHudEditKey = KeyBindingHelper.registerKeyBinding(new KeyBinding(
-            "key.boogerclient.hud_edit",
-            InputUtil.Type.KEYSYM,
-            GLFW.GLFW_KEY_F6,
-            CATEGORY
-        ));
-
         HudRenderCallback.EVENT.register((drawContext, tickCounter) -> {
             hudManager.render(drawContext, tickCounter.getDynamicDeltaTicks());
         });
 
         ClientTickEvents.END_CLIENT_TICK.register(client -> {
             pvpManager.tick(client);
-            if (openMenuKey.wasPressed()) {
-                client.setScreen(new com.boogerclient.gui.BoogerMenuScreen());
-            }
-            if (toggleHudEditKey.wasPressed()) {
-                hudManager.toggleEditMode();
-            }
         });
 
         LOGGER.info("Booger Client initialized.");
